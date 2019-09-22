2faeb63ac5dabac6f42668330d85142c522a0e94
vrajpatell <51841184+vrajpatell@users.noreply.github.com>
2019-09-22 16:10:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import datetime
 app = Flask(__name__)
 port = int(os.getenv("PORT", 5000))
 
-server = 'vhpserver.database.windows.net'
-database = 'vhpdatabase'
-username = 'vrajpatell'
-password = 'Vraj0712.'
+server = 'Server Name'
+database = 'database name'
+username = 'user name'
+password = 'password'
 driver= '{ODBC Driver 17 for SQL Server}'
 
 cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
